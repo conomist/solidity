76580aad2ad92f50098056925a3824752ec7cff6
Matheus Aguiar <matheus.pit@gmail.com>
2026-01-23 16:20:10 -0300

    fixup! Add and update tests

--- test/libsolidity/semanticTests/builtinFunctions/erc7201_array_string_literal.sol
+++ test/libsolidity/semanticTests/builtinFunctions/erc7201_array_string_literal.sol
@@ -1,7 +1,7 @@
 contract C {
-    function test() public pure returns (uint) {
-        return erc7201(["x"][0]);
+    function test() public pure returns (bool) {
+        return erc7201(["x"][0]) == erc7201(["y", "x"][1]);
     }
 }
 // ----
-// test() -> -54410875867715754317031553053437925074622998971851806468326073383238108491776
+// test() -> true
--- test/libsolidity/semanticTests/builtinFunctions/erc7201_ternary_operator.sol
+++ test/libsolidity/semanticTests/builtinFunctions/erc7201_ternary_operator.sol
@@ -1,7 +1,11 @@
 contract C {
-    function test() public pure returns (uint) {
+    function simple() public pure returns (uint) {
         return erc7201(true ? "x" : "y");
+    }
+    function compounded(bool c1, bool c2) public pure returns (uint) {
+        return erc7201(c1 ? "a" : (c2 ? "x" : "c"));
     }
 }
 // ----
-// test() -> -54410875867715754317031553053437925074622998971851806468326073383238108491776
+// simple() -> -54410875867715754317031553053437925074622998971851806468326073383238108491776
+// compounded(bool,bool): false, true -> -54410875867715754317031553053437925074622998971851806468326073383238108491776
